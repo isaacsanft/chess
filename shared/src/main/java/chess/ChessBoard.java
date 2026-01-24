c85a0e740cc1ceeb678ae02d8ea0093c10e877b5
isaacsanft <itsanft@gmail.com>
2026-01-23 20:11:40 -0700

    Reset board tests now pass

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -39,6 +42,39 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        this.squares = new ChessPiece[8][8];
+        for (int i = 1; i < 9; i ++) {
+            ChessPosition pos_black = new ChessPosition(7, i);
+            ChessPosition pos_white = new ChessPosition(2, i);
+            ChessPiece piece_black = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+            ChessPiece piece_white = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+            addPiece(pos_black, piece_black);
+            addPiece(pos_white, piece_white);
+        }
+        ChessPiece.PieceType[] row_order = {
+                ChessPiece.PieceType.ROOK, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.BISHOP, ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.KING, ChessPiece.PieceType.BISHOP, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.ROOK
+        };
+        for (int i = 1; i <= 8; i++) {
+            ChessPosition pos_white = new ChessPosition(1, i);
+            ChessPosition pos_black = new ChessPosition(8, i);
+            ChessPiece.PieceType type = row_order[i - 1];
+            addPiece(pos_white, new ChessPiece(ChessGame.TeamColor.WHITE, type));
+            addPiece(pos_black, new ChessPiece(ChessGame.TeamColor.BLACK, type));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -8,6 +8,7 @@ import chess.piececalculators.PawnMovesCalculator;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -79,5 +80,19 @@ public class ChessPiece {
             return new PawnMovesCalculator().calculate_moves(board, myPosition);
         }
         return List.of();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 }
