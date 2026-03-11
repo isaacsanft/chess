3f470fd826e1cce9e89921d894b910acee7cbbd6
isaacsanft <itsanft@gmail.com>
2026-03-11 00:11:38 -0600

    Code Quality.

--- shared/src/main/java/model/User.java
+++ shared/src/main/java/model/User.java
@@ -1,7 +1,4 @@
 package model;
 
 public record User(String username, String password, String email) {
-    User update(String newUsername, String newPassword, String newEmail) {
-        return new User(newUsername, newPassword, newEmail);
-    }
 }
