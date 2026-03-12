9a2741b804f46650598137440691d55290e49ad5
isaacsanft <itsanft@gmail.com>
2026-03-11 22:58:00 -0600

    MYSQL AUTH DAO class done

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -11,16 +11,32 @@ public class MySQLAuthDAO implements AuthDAO {
 
     @Override
     public AuthToken getAuth(String auth) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var query = "SELECT authToken, username FROM auth WHERE authToken=?";
+            try (var ps = conn.prepareStatement(query)) {
+                ps.setString(1, auth);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        AuthToken authToken = new AuthToken(rs.getString("authToken"), rs.getString("username"));
+                        return authToken;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
     @Override
     public void deleteAuth(String auth) throws DataAccessException {
-
+        var query = "DELETE FROM auth WHERE authToken=?";
+        DatabaseManager.executeUpdate(query, auth);
     }
 
     @Override
     public void clear() throws DataAccessException {
-
+        var query = "TRUNCATE auth";
+        DatabaseManager.executeUpdate(query);
     }
 }
